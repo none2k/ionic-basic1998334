ca3b57bc13efe8e8007866663bf0cc5b8882bbf3
Patricio <124222581+none2k@users.noreply.github.com>
2023-10-30 18:51:54 -0600

    Practica 3

--- tarea-1/src/app/app-routing.module.ts
+++ tarea-1/src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'alumnos',
     pathMatch: 'full'
+  },
+  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
   },
 ];
 
